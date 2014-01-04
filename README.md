1a78cef05bec57a47ce840d9e68112ca4d1ed7ec
Daniel Drown <dan-android@drown.org>
2014-01-04 17:35:03 -0600

    ipv6 and tcp headers

---
+++ README.md
@@ -0,0 +1,3 @@
+Include files missing from the android-ndk, for use in android-ports
+
+more info at: http://dan.drown.org/android/src/
---
+++ netinet/tcp-defines.h
@@ -0,0 +1,36 @@
+/* Copyright (C) 1991-1997, 2001, 2003, 2006 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+/* originally from netinet/tcp.h */
+
+#ifndef _NETINET_TCP_DEFINES_H
+#define _NETINET_TCP_DEFINES_H
+
+#define	TCP_MAXSEG	 2	/* Set maximum segment size  */
+
+# define TCPOPT_NOP		1
+# define TCPOPT_MAXSEG		2
+# define TCPOLEN_MAXSEG		4
+# define TCPOPT_WINDOW		3
+# define TCPOLEN_WINDOW		3
+# define TCPOPT_SACK_PERMITTED	4		/* Experimental */
+# define TCPOLEN_SACK_PERMITTED	2
+# define TCPOPT_TIMESTAMP	8
+# define TCPOLEN_TIMESTAMP	10
+
+#endif /* _NETINET_TCP_DEFINES_H */
